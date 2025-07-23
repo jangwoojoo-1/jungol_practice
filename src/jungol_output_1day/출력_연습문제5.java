24237545abd79340442318f57f6f9c3f4161677a
jangwoojoo-1 <jwscape01@gmail.com>
2025-07-24 00:13:09 +0900

    jungol_practice_output_day1_first_commit

---
+++ src/jungol_output_1day/출력_연습문제5.java
@@ -0,0 +1,9 @@
+package jungol_output_1day;
+
+public class 출력_연습문제5 {
+    public static void main(String[] args) {
+        String str1 = "서식 문자로 출력하는 방법입니다.";
+        System.out.printf("%s\n", str1);
+        System.out.printf("%s", "이것은 문자열을 나타냅니다.");
+    }
+}
---
+++ src/jungol_output_1day/출력_자가진단4.java
@@ -0,0 +1,9 @@
+package jungol_output_1day;
+
+public class 출력_자가진단4 {
+    public static void main(String[] args) {
+        System.out.println("(@) (@)");
+        System.out.println("(=^.^=)");
+        System.out.println("(-m-m-)");
+    }
+}
