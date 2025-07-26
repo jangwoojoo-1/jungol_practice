01ff5d311cf8f1b4ab95cbddca5b35ebc32d98af
jangwoojoo-1 <jwscape01@gmail.com>
2025-07-26 21:27:22 +0900

    day4 first commit

---
+++ src/jungol/jungol_loop2/반복제어문2_형성평가10.java
@@ -0,0 +1,24 @@
+package jungol.jungol_loop2;
+
+import java.util.Scanner;
+
+public class 반복제어문2_형성평가10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        while(num1<2 && num2<2 && num1>9 && num2>9){
+            System.out.println("2부터 9까지 중 2개를 입력하세요.");
+            num1 = sc.nextInt();
+            num2 = sc.nextInt();
+        }
+
+        for(int i = 1 ; i <= 9 ; i++){
+            for(int j = num1 ; j >= num2  ; j--){
+                System.out.printf("%d * %d =%3d   ", j, i, j*i);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/jungol/jungol_loop2/반복제어문2_형성평가7.java
@@ -0,0 +1,14 @@
+package jungol.jungol_loop2;
+
+import java.util.Scanner;
+
+public class 반복제어문2_형성평가7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i = 1 ; i <= 10 ; i++){
+            System.out.printf("%d ", num*i);
+        }
+    }
+}
